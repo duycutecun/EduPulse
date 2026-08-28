65ad12bf769ee90a0ccbe70a0fbb94a298f5519c
EduPulse Dev <edupulse@situ.vn>
2026-08-28 12:13:15 +0700

    fix(android): update compileSdk to 36 for file_picker

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.edu.edupulse"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
